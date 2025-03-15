c373a2a8d30abc8f0dd6862fe3b6c5708230a926
falcram <dubovikarseniy15@gmail.com>
2025-03-15 14:47:58 +0000

    small corrects

--- maindocker/view/cpu/cpu_usage.sh
+++ maindocker/view/cpu/cpu_usage.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 while true; do
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
-
-    if [ $? -ne 0 ]; then
-        echo "Failed to get CPU usage. Exiting."
-        exit 1
-    fi
 
     echo "CPU Usage: $cpu_usage%" > /var/www/cpu/cpu_usage.txt
 
